79a00d888bccada8efb56a96adeabc1a5a1b2679
Kim_min_woo <alsdnsla44@naver.com>
2021-06-20 22:23:50 +0900

    template ~ing

--- main.go
+++ main.go
@@ -1,31 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"os"
 	"strings"
+	"text/template"
 
 	"github.com/labstack/echo"
 	"github.com/purplephone/learngo/scrapper"
 )
+
+var term string
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
-	defer os.Remove("jobs.csv")
-	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	scrapper.Scrape(term)
-	return c.Attachment("jobs.csv", "jobs.csv")
+	term = strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+
+	return process()
+}
+
+func process() error {
+	str := scrapper.Scrape(term)
+	const tmp1 = `문제
+	{{.Description}}
+	입력
+	{{.Input}}
+	출력
+	{{.Output}}
+	`
+	t, err := template.New("t1").Parse(tmp1)
+	if err != nil {
+		panic(err)
+	}
+	return t.Execute(os.Stdout, str)
 }
 
 func main() {
-	/*e := echo.New()
+	e := echo.New()
 	e.GET("/", handleHome)
 	e.POST("/scrape", handleScrape)
-	e.Logger.Fatal(e.Start(":1323"))*/
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Scan()
-	scrapper.Scrape(sc.Text())
+	e.Logger.Fatal(e.Start(":1323"))
 }
--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -1,26 +1,25 @@
 package scrapper
 
 import (
-	"fmt"
+	"bufio"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-type extractedJob struct {
-	id       string
-	title    string
-	location string
-	salary   string
-	summary  string
+type Baek struct {
+	Description string
+	Input       string
+	Output      string
 }
 
 var baseURL string = "https://www.acmicpc.net/problem/"
 
 //baekjun problem scrape
-func Scrape(num string) {
+func Scrape(num string) Baek {
 	//set URL
 	URL := baseURL + num
 
@@ -37,18 +36,30 @@ func Scrape(num string) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
+	var b Baek
 	//Find the items
 	doc.Find("#problem-body").Each(func(i int, s *goquery.Selection) {
-		description := s.Find("#problem_description").Text()
-		input := s.Find("#problem_input").Text()
-		output := s.Find("#problem_output").Text()
-		description = strings.TrimSpace(description)
-		input = strings.TrimSpace(input)
-		output = strings.TrimSpace(output)
-		fmt.Println("문제\n" + description)
-		fmt.Println("입력\n" + input)
-		fmt.Println("출력\n" + output)
+		description := strings.TrimSpace(s.Find("#problem_description").Text())
+		input := strings.TrimSpace(s.Find("#problem_input").Text())
+		output := strings.TrimSpace(s.Find("#problem_output").Text())
+		b = Baek{description, input, output}
+		//writeBaek(b)
 	})
+	//ba := "문제\n" + b.description + "\n\n입력\n" + b.input + "\n\n출력\n" + b.output
+	return b
+}
+
+func writeBaek(b Baek) {
+	file, err := os.Create("baekjun.hwp")
+	checkErr(err)
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	defer w.Flush()
+
+	ba := "문제\n" + b.Description + "\n\n입력\n" + b.Input + "\n\n출력\n" + b.Output
+	_, wErr := w.WriteString(ba)
+	checkErr(wErr)
 }
 
 func checkErr(err error) {
